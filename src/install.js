32ffe34b8635f27e2c92fc751e8636af7a33835f
helena.almeida <helenaifalmeida@gmail.com>
2022-03-22 15:01:13 +0000

    testing main location

--- src/install.js
+++ src/install.js
@@ -1,16 +1,9 @@
 import VueAmountFormatInput from './components/VueAmountFormatInput.vue'
 
-const AmountFormatInputInstall = {
+export default {
 	install(Vue, options) {
 		// Let's register our component globally
 		/* Plugin code goes here */
 		Vue.component('vue-amount-format-input', VueAmountFormatInput)
 	}
-};
-
-// Automatic installation if Vue has been added to the global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(AmountFormatInputInstall)
 }
-
-export default AmountFormatInputInstall
